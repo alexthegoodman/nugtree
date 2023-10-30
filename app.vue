6c98681d43b6d757fb801d0e72241c3f5e81a759
Alex Goodman <alexthegoodman@gmail.com>
2023-10-30 14:02:01 -0400

    vue flow

--- app.vue
+++ app.vue
@@ -1,13 +1,51 @@
+<script setup>
+import { VueFlow, useVueFlow } from "@vue-flow/core";
+
+const defaultNode = {
+  style: {
+    backgroundColor: "grey",
+  },
+  width: 100,
+  height: 100,
+};
+
+const nodes = ref([
+  {
+    ...defaultNode,
+    id: "1",
+    position: { x: 50, y: 50 },
+    label: "Node 1",
+  },
+  {
+    ...defaultNode,
+    id: "3",
+    position: { x: 250, y: 50 },
+    label: "Node 3",
+  },
+  {
+    ...defaultNode,
+    id: "4",
+    position: { x: 350, y: 50 },
+    label: "Node 4",
+  },
+]);
+
+const { addNodes } = useVueFlow({
+  nodes,
+});
+</script>
+
 <template>
   <UContainer>
     <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
-        </div>
-      </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
+      <div style="height: 700px">
+        <VueFlow />
+        <ul class="mt-5">
+          <li v-for="node in nodes" :key="node.id">
+            {{ node.id }}: {{ node.position }}
+          </li>
+        </ul>
+      </div>
     </UCard>
   </UContainer>
 </template>
